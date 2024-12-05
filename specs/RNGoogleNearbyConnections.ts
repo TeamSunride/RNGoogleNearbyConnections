f9320170ebd90629875e5d68179a427aa0b82e83
Starman <30315137+Starman3787@users.noreply.github.com>
2024-12-05 17:19:41 +0000

    update typings

--- specs/RNGoogleNearbyConnections.ts
+++ specs/RNGoogleNearbyConnections.ts
@@ -3,6 +3,10 @@ import {TurboModuleRegistry} from 'react-native';
 
 export interface Spec extends TurboModule {
   initConnectionManager(serviceId: string, strategy: string): void;
+  startDiscovery(): void;
+  stopDiscovery(): void;
+  startAdvertising(identifier: string): void;
+  stopAdvertising(): void;
 }
 
 export default TurboModuleRegistry.getEnforcing<Spec>(
